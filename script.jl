e4a39c24a2a2007a5e19d5e7eccfe832af44b2fe
Júlio Hoffimann <julio.hoffimann@gmail.com>
2024-11-15 10:01:08 -0300

    Add script.jl

---
+++ script.jl
@@ -0,0 +1,42 @@
+## GEOSPATIAL DATA SCIENCE WITH JULIA
+
+
+
+## 1. Loading meteorological data
+
+
+
+## 2. Projecting data to Euclidean space
+
+
+
+## 3. Transforming values to normal scores
+
+
+
+## 4. Fitting variogram/covariance model
+
+
+
+## 5. Defining grid for interpolation
+
+
+
+## 6. Kriging interpolation
+
+
+
+## 7. Reverting from normal scores
+
+
+
+## 8. Reprojecting to Earth's ellipsoid
+
+
+
+## 9. Loading base map for visualization
+
+
+
+## 10. Interactive 3D visualization
+
